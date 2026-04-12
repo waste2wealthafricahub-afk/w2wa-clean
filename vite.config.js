57e9c7b338e430bcd6b903322164833fee314635
Tunde Solarin <waste2wealthafricahub@gmail.com>
2026-04-12 13:21:58 +0100

    Fix Vercel build configuration

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1000,
-  },
+  plugins: [react()]
 });
