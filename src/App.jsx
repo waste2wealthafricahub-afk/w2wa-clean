f5dda3742af661ccc6688d159f81ca9f1760f0bd
Tunde Solarin <waste2wealthafricahub@gmail.com>
2026-04-12 09:23:22 +0100

    Fix case-sensitive import in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import AdminDashboard from "./pages/AdminDashboard";
 import SchoolDashboard from "./pages/SchoolDashboard";
 import RepDashboard from "./pages/RepDashboard";
