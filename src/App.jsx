71007ae55aec853430b59c3ed07838d7384d5641
Tunde Solarin <waste2wealthafricahub@gmail.com>
2026-08-08 17:41:54 +0100

    Fix application routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,123 +5,107 @@ import {
 } from "react-router-dom";
 
 import Login from "./pages/Login";
-
 import SchoolRegistration from "./pages/SchoolRegistration";
-
 import RepresentativeRegistration from "./pages/RepresentativeRegistration";
+import MonitorRegistration from "./pages/MonitorRegistration";
 
 import SchoolDashboard from "./pages/SchoolDashboard";
-
+import SchoolWalletDashboard from "./pages/SchoolWalletDashboard";
 import RepresentativeDashboard from "./pages/RepresentativeDashboard";
 
 import AdminDashboard from "./pages/AdminDashboard";
+import MonitoringDashboard from "./pages/MonitoringDashboard";
+import UserManagement from "./pages/UserManagement";
 
 import ProtectedRoute from "./components/ProtectedRoute";
-import SchoolWalletDashboard from "./pages/SchoolWalletDashboard";
-import MonitoringDashboard from "./pages/MonitoringDashboard";
-import MonitorRegistration from "./pages/MonitorRegistration";
-import UserManagement from "./pages/UserManagement";
+
 export default function App() {
-
   return (
-
     <BrowserRouter>
-
       <Routes>
 
         {/* LOGIN */}
-
         <Route
           path="/"
           element={<Login />}
         />
 
         {/* REGISTRATIONS */}
-
         <Route
           path="/school-registration"
-          element={
-            <SchoolRegistration />
-          }
+          element={<SchoolRegistration />}
         />
-<Route
-  path="/monitor-registration"
-  element={
-    <MonitorRegistration />
-  }
-/>
+
         <Route
           path="/rep-registration"
-          element={
-            <RepresentativeRegistration />
-          }
+          element={<RepresentativeRegistration />}
+        />
+
+        <Route
+          path="/monitor-registration"
+          element={<MonitorRegistration />}
         />
 
         {/* SCHOOL */}
-
         <Route
           path="/school-dashboard"
           element={
-            <ProtectedRoute
-              allowedRole="school"
-            >
+            <ProtectedRoute allowedRole="school">
               <SchoolDashboard />
             </ProtectedRoute>
           }
         />
+
         <Route
           path="/school-wallet"
           element={
-          <ProtectedRoute allowedRole="school">
-          <SchoolWalletDashboard />
-          </ProtectedRoute>
-           }
+            <ProtectedRoute allowedRole="school">
+              <SchoolWalletDashboard />
+            </ProtectedRoute>
+          }
         />
-        {/* REPRESENTATIVE */}
 
+        {/* REPRESENTATIVE */}
         <Route
           path="/rep-dashboard"
           element={
-            <ProtectedRoute
-              allowedRole="representative"
-            >
+            <ProtectedRoute allowedRole="representative">
               <RepresentativeDashboard />
             </ProtectedRoute>
           }
         />
 
         {/* ADMIN */}
-
         <Route
           path="/admin-dashboard"
           element={
-            <ProtectedRoute
-              allowedRole="admin"
-            >
+            <ProtectedRoute allowedRole="admin">
               <AdminDashboard />
             </ProtectedRoute>
           }
         />
-<Route
-  path="/monitoring-dashboard"
-  element={
-    <ProtectedRoute
-     allowedRole="monitor"
-    >
-      <MonitoringDashboard />
-    </ProtectedRoute>
-  }
-/>
-<Route
-  path="/user-management"
-  element={
-    <ProtectedRoute allowedRole="admin">
-      <UserManagement />
-    </ProtectedRoute>
-  }
-/>
-      </Routes>
 
+        {/* MINISTRY MONITOR */}
+        <Route
+          path="/monitoring-dashboard"
+          element={
+            <ProtectedRoute allowedRole="monitor">
+              <MonitoringDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* USER MANAGEMENT */}
+        <Route
+          path="/user-management"
+          element={
+            <ProtectedRoute allowedRole="admin">
+              <UserManagement />
+            </ProtectedRoute>
+          }
+        />
+
+      </Routes>
     </BrowserRouter>
   );
 }
